f7762a23849d373b3af3e62e97cbcd29fcada83d
Jorge Illera RIvera <128382772+Illera03@users.noreply.github.com>
2025-11-11 16:11:04 +0100

    Refactor sum function and variable names

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-#archivo
-# un fichero de código
-# Python que contenga lo siguiente:
-# • Una función que sume 2 números.
-# • Dos variables con 2 números (a vuestra elección).
-# • Un comando print para mostrar la suma de los 2 números usando
-# la función.
-
-def sumar(num1, num2):
-    return num1 + num2
-numero1 = 5
-numero2 = 10
-print(sumar(numero1, numero2))
+def sumar(a, b):
+    return a + b
+a = 5
+b = 10
+print(sumar(a, b))
