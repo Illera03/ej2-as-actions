9d5a9d2c656addc4d81009e434e09b3ad3465486
Jorge <jillera10@gmail.com>
2025-11-11 16:04:32 +0100

    main

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 #archivo
+# un fichero de código
+# Python que contenga lo siguiente:
+# • Una función que sume 2 números.
+# • Dos variables con 2 números (a vuestra elección).
+# • Un comando print para mostrar la suma de los 2 números usando
+# la función.
+
+def sumar(num1, num2):
+    return num1 + num2
+numero1 = 5
+numero2 = 10
+print(sumar(numero1, numero2))
